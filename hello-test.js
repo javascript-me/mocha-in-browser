fdd43191fae93ac4bf3a6d8f3b6a2718de3b6063
agileabc <agileabc@sina.com>
2016-01-13 16:43:13 +0800

    Create hello-test.js

---
+++ hello-test.js
@@ -0,0 +1,18 @@
+var container = document.createElement("div");
+
+describe('#innerHTML', function () {
+
+  before(function(){
+    document.body.appendChild(container)
+  })
+
+  after(function(){
+    document.body.removeChild(container)
+  })
+
+  it('should show what I input', function () {
+    container.innerHTML = "<li>aaa</li>"
+    chai.expect(container.innerHTML).to.equal("<li>aaa</li>");
+  });
+
+});
